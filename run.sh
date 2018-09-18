ec55a8eae63374548167486ac81a6e6306d0afcc
Radu Cojocaru <radu.cojocaru@tremend.com>
2018-09-18 15:06:25 +0300

    Add script file in properties section

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SCRIPT_FILE=$WERCKER_CREATE_JOB_SCRIPT_FILE
 JSON_FILE=$WERCKER_CREATE_JOB_JSON_FILE
 DCOS_URL=$WERCKER_CREATE_JOB_DCOS_URL
 TIME_LIMIT=$WERCKER_CREATE_JOB_TIME_LIMIT
 
+echo "$SCRIPT_FILE"
 echo "$JSON_FILE"
 echo "$DCOS_URL"
 echo "$TIME_LIMIT"
@@ -12,7 +14,7 @@ ls
 
 pwd
 
-if python script.py "$JSON_FILE" "$DCOS_URL" "$TIME_LIMIT"; then
+if python "$SCRIPT_FILE" "$JSON_FILE" "$DCOS_URL" "$TIME_LIMIT"; then
     fail "\nJob error"
 fi
 
