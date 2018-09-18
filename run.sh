14dc0731386cdf3e1fd3be9871347fc229b6e949
Radu Cojocaru <radu.cojocaru@tremend.com>
2018-09-18 14:37:26 +0300

    Store properties values

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "TEST"
 
-echo "$WERCKER_CREATE_JOB_JSON_FILE"
-echo "$WERCKER_CREATE_JOB_DCOS_URL"
-echo "$WERCKER_CREATE_JOB_TIME_LIMIT"
+JSON_FILE=$WERCKER_CREATE_JOB_JSON_FILE
+DCOS_URL=$WERCKER_CREATE_JOB_DCOS_URL
+TIME_LIMIT=$WERCKER_CREATE_JOB_TIME_LIMIT
+
+echo "$JSON_FILE"
+echo "$DCOS_URL"
+echo "$TIME_LIMIT"
 
 echo "$(python --version)"
 success "\nTest triggered successfully."
