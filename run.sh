33cc58568591b0980ec7cfc6b8b17ce5cccc5b54
Radu Cojocaru <radu.cojocaru@tremend.com>
2018-09-18 14:11:33 +0300

    Modify run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ echo "$WERCKER_CREATE_JOB_JSON_FILE"
 echo "$WERCKER_CREATE_JOB_DCOS_URL"
 echo "$WERCKER_CREATE_JOB_TIME_LIMIT"
 
-echo "python --version"
+echo "$(python --version)"
 success "\nTest triggered successfully."
